d4a553819cd4f849eb8c812899d76a50b5febec8
mica <mica@MacBook-Pro-de-BCTECNOLOGIA.local>
2025-08-28 20:26:56 -0400

    configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +11,9 @@ import { RankingModule } from './ranking/ranking.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     ScheduleModule.forRoot(),
     PrismaModule,
     YouTubeModule,
