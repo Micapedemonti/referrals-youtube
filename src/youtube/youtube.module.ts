3a0896f35d841904c32a790edce3d57e2e86166d
mica <mica@MacBook-Pro-de-BCTECNOLOGIA.local>
2025-08-30 10:00:22 -0400

    added webhook suscription

--- src/youtube/youtube.module.ts
+++ src/youtube/youtube.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { YouTubeService } from './youtube.service';
 import { YouTubeController } from './youtube.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { YouTubeWebhookController } from './youtube-webhook.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [YouTubeService],
-  controllers: [YouTubeController],
+  controllers: [YouTubeController,YouTubeWebhookController],
   exports: [YouTubeService],
 })
 export class YouTubeModule {} 
